ab34ed76000240f6ff33a43527482f57c08b03f1
admin <kurlan.bernal@gmail.com>
2022-11-28 23:53:23 -0600

    create spring project

---
+++ src/main/java/com/test/springboottest/controller/TestRest.java
@@ -0,0 +1,13 @@
+package com.test.springboottest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestRest {
+
+	@GetMapping("/hello-world")
+	public String helloWorld() {
+		return "Hello World";
+	}
+}
